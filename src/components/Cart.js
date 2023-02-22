fb422efa3ea3faccc222a77062ded694c2acd6ce
tobuya <thomasobuya95@gmail.com>
2023-02-22 17:56:23 +0300

    Handle states for Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../store/cartSlice";
 import "./Cart.css";
+
 const Cart = () => {
-  const quantity = 5;
+  const quantity = useSelector((state) => state.cart.totalQuantity);
+  const dispatch = useDispatch();
+  const showCart = () => {
+    dispatch( cartActions.setShowCart());
+  }
+  
   return (
     <div className="cartIcon">
-      <h3>Cart: {quantity} Items</h3>
+      <h3 onClick={showCart}>Cart: {quantity} Items</h3>
     </div>
   );
 };
