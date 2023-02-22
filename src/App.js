cc4a46d53efc6135b12afa982ec5076d41f53fd0
tobuya <thomasobuya95@gmail.com>
2023-02-22 17:47:44 +0300

    Import Auth and Layout components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const cartItems = useSelector((state) => state.cart.itemsList);
+  console.log(cartItems);
+
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {!isLoggedIn && <Auth /> }
+      {isLoggedIn && <Layout /> }
     </div>
   );
 }
